f5ea767451de05b404669643a8ad419fa3f23000
Manu <mandrescabral@gmail.com>
2024-07-12 17:17:44 -0300

    feat: add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import type { ButtonProps } from '../global'
+
+export default function Button({
+    children,
+    onClick,
+    className,
+    type = 'primary',
+}: ButtonProps): JSX.Element {
+    const types = {
+        primary:
+            'text-white bg-te-100 hover:bg-te-200 focus:ring-4 focus:outline-none focus:ring-te-400 dark:focus:ring-te-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2',
+        secondary:
+            'text-te-100 bg-white hover:bg-te-100 focus:ring-4 focus:outline-none focus:ring-te-400 dark:focus:ring-te-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2',
+    }
+    return (
+        <button onClick={onClick} className={`${types[type]} ${className}`}>
+            {children}
+        </button>
+    )
+}
