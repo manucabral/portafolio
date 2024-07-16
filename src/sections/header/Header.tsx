9b13fc3f98de46cad2b5247ad8fc56116e15e26c
Manu <mandrescabral@gmail.com>
2024-07-16 16:21:14 -0300

    refactor(header): update background opacity for improved design consistency

--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 
 export default function Header({ sectionRefs }: HeaderProps): JSX.Element {
     return (
-        <header className="sticky top-0 z-50 flex justify-center items-center bg-se sm:bg-opacity-50">
+        <header className="sticky top-0 z-50 flex justify-center items-center bg-se bg-opacity-80 sm:bg-opacity-50">
             <Navbar sectionRefs={sectionRefs} />
         </header>
     )
