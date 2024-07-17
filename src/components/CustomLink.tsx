7cd33ca219bd15e80c2cd51ed81b4552f5bfdaac
Manu <mandrescabral@gmail.com>
2024-07-16 21:33:03 -0300

    feat: add CustomLink component

---
+++ src/components/CustomLink.tsx
@@ -0,0 +1,22 @@
+import { CustomLinkProps } from '../global'
+import LinkIcon from './icons/LinkIcon'
+
+export default function CustomLink({
+    href,
+    className,
+    target,
+    children,
+}: CustomLinkProps): JSX.Element {
+    const defaultClassName = `text-te-400 hover:underline flex items-center gap-1 ${className}}`
+    return (
+        <a
+            href={href}
+            className={defaultClassName}
+            target={target}
+            rel="noreferrer"
+        >
+            <LinkIcon name="Link" />
+            {children}
+        </a>
+    )
+}
